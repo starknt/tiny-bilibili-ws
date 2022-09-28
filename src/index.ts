5c2dcc89e967a2ec06be3b7601b43e572912e6c9
starknt <1431880400@qq.com>
2022-09-28 11:39:11 +0800

    chore: export  `TCPOptions` type

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ export { KeepLiveTCP, KeepLiveWS, getLongRoomId } from './node'
 export { WS_OP, WS_BODY_PROTOCOL_VERSION } from './buffer'
 export type { Protocol, ProtocolHeader } from './buffer'
 export type {
-  WSOptions, MESSAGE_CMD, MessageMeta, Message, ListenerEvents, RoomResponse,
+  WSOptions, TCPOptions, MESSAGE_CMD, MessageMeta, Message, ListenerEvents, RoomResponse,
   DANMU_MSG, SEND_GIFT_MSG, WATCHED_CHANGE_MSG,
 } from './types'
