8c88b51b2140c072a44b6b282d910b4a3550ba25
starknt <1431880400@qq.com>
2022-09-30 02:42:50 +0800

    refactor: `cmd` MSG

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ export { WS_OP, WS_BODY_PROTOCOL_VERSION } from './buffer'
 export type { Protocol, ProtocolHeader } from './buffer'
 export type {
   WSOptions, TCPOptions, MESSAGE_CMD, MessageMeta, Message, ListenerEvents, RoomResponse,
-  DANMU_MSG, SEND_GIFT_MSG, WATCHED_CHANGE_MSG,
 } from './types'
+export * from './cmd'
