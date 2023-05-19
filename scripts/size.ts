f0961c3dba27454c1241de8991716e9581af7d95
starknt <1431880400@qq.com>
2023-05-20 01:42:55 +0800

    chore(script): modify log info

--- scripts/size.ts
+++ scripts/size.ts
@@ -15,12 +15,12 @@ let minified = ''
 const browsercode = await fs.readFile(browser, 'utf8')
 minified += (await minify(browsercode)).code
 console.log()
-console.log(`browser gzip    ${(gzip(minified) / 1024).toFixed(2)} KiB`)
-console.log(`browser brotli  ${(brotli(minified) / 1024).toFixed(2)} KiB`)
+console.log(`BROWSER entry gzip    ${(gzip(minified) / 1024).toFixed(2)} KiB`)
+console.log(`BROWSER entry brotli  ${(brotli(minified) / 1024).toFixed(2)} KiB`)
 
 minified = ''
 const code = await fs.readFile(node, 'utf8')
 minified += (await minify(code)).code
 console.log()
-console.log(`node gzip    ${(gzip(minified) / 1024).toFixed(2)} KiB`)
-console.log(`node brotli  ${(brotli(minified) / 1024).toFixed(2)} KiB`)
+console.log(`NODEJS entry gzip    ${(gzip(minified) / 1024).toFixed(2)} KiB`)
+console.log(`NODEJS entry brotli  ${(brotli(minified) / 1024).toFixed(2)} KiB`)
