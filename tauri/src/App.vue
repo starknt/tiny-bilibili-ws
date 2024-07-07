f764edfef9c94619d1b212a56d0010d594a9a6ba
starknt <1431880400@qq.com>
2024-07-07 19:50:39 +0800

    test: add `tauri`

---
+++ tauri/src/App.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import { KeepLiveWS } from 'tiny-bilibili-ws/browser'
+import { onMounted, ref } from 'vue'
+import { fetch } from '@tauri-apps/plugin-http'
+
+const danmu = ref<any[]>([])
+
+function randomElement<T>(list: T[]): T {
+  return list[Math.floor(Math.random() * list.length)]
+}
+
+onMounted(async () => {
+  const room = await fetch(`https://api.live.bilibili.com/room/v1/Room/mobileRoomInit?id=${import.meta.env.VITE_ROOM}`)
+    .then(res => res.json())
+  const conf = await fetch(`https://api.live.bilibili.com/room/v1/Danmu/getConf?room_id=${room.data.room_id}&platform=pc&player=web`)
+    .then(res => res.json())
+
+  const host = randomElement(conf.data.host_server_list)
+
+  const live = new KeepLiveWS(room.data.room_id, {
+    url: `wss://${host.host}:${host.wss_port}/sub`,
+    key: conf.data.token,
+  })
+
+  live.runWhenConnected(() => {
+    console.log(`正在监听 ${import.meta.env.VITE_ROOM}`)
+  })
+
+  live.on('DANMU_MSG', async (m) => {
+    console.log('Online: ', await live.getOnline())
+
+    danmu.value.push(m)
+  })
+})
+</script>
+
+<template>
+  <div v-for="m in danmu" :key="m">
+    {{ JSON.stringify(m.data) }}
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
